99932059d452bc593bd70cd163906a988cb19a4c
EricReyes <ericreyesmilian05@gmail.com>
2026-04-19 03:01:58 +0000

    presuntamente arreglado el bug al cambiar de pthread a mthread

--- minix/lib/libmthread/pthread_compat.c
+++ minix/lib/libmthread/pthread_compat.c
@@ -55,11 +55,14 @@ int pthread_mutex_lock(pthread_mutex_t *mutex)
  *===========================================================================*/
 int pthread_mutex_trylock(pthread_mutex_t *mutex)
 {
+	
+
 	if (PTHREAD_MUTEX_INITIALIZER == *mutex) {
 		mthread_mutex_init(mutex, NULL);	
 	}
+	
+	return mthread_mutex_trylock(mutex);
 
-	return pthread_mutex_trylock(mutex);
 }
 
 /*===========================================================================*
