28613ce21cfd5c39d2607caf7fc60660a0a6d444
KamAlien <kamilareinoso25junio2005@gmail.com>
2026-04-24 06:04:31 -0400

    Intento de Tree

---
+++ minix/commands/tree/tree.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/stat.h>
+
+void tree(char *dir)
+{
+    DIR *directorio = opendir(dir);
+
+    if(directorio == NULL)
+    {
+        perror("Error al abrir el directorio");
+        return;
+    }
+
+     struct dirent *hs;
+
+    while ((hs = readdir(directorio)) != NULL)
+    {
+        if((hs -> d_name[0] == "." && hs -> d_name[1] == '\0') || (hs -> d_name[0] == '.' && hs -> d_name[1] == '.' && hs -> d_name[2] == '\0'))
+        {
+            continue;
+        }
+        printf("%s\n", hs->d_name);
+    }
+    closedir(directorio);
+}
+
+int main(int argc, char *argv[])
+{
+    // Esto es para el directorio actual
+    if(argc == 1)
+    {
+        tree(NULL);
+    }
+
+    // Esto es para un directorio específico
+    else
+    {
+        tree(argv[1]);
+    }
+}
